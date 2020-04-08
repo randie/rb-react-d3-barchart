dd1bca73b9e352eb2ab4e175a0cd1a3d403585bc
Randie B <randie.bemis@gmail.com>
2020-04-08 00:30:01 -0700

    add rects to D3 chart

--- src/components/d3-chart.js
+++ src/components/d3-chart.js
@@ -1,5 +1,7 @@
 import * as d3 from 'd3';
 
+const data = [20, 12, 16, 25, 10];
+
 export default class D3Chart {
   constructor(element) {
     const svg = d3
@@ -7,5 +9,16 @@ export default class D3Chart {
       .append('svg')
       .attr('width', 500)
       .attr('height', 500);
+
+    svg
+      .selectAll('rect')
+      .data(data)
+      .enter()
+      .append('rect')
+      .attr('x', (d, i) => i * 100)
+      .attr('y', 50)
+      .attr('width', 50)
+      .attr('height', d => d * 10)
+      .attr('fill', 'lightgrey');
   }
 }
