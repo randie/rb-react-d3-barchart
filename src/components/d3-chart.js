61b15080f496573f27bbb5df1a0e6bbf41e4b70e
Randie B <randie.bemis@gmail.com>
2020-04-11 00:57:23 -0700

    retrieve data from fake rest api and use that data to affect DOM elements' attributes

--- src/components/d3-chart.js
+++ src/components/d3-chart.js
@@ -1,24 +1,22 @@
 import * as d3 from 'd3';
 
-const data = [20, 12, 16, 25, 10];
+const url = 'http://localhost:4000/ages';
 
 export default class D3Chart {
   constructor(element) {
-    const svg = d3
-      .select(element.current)
-      .append('svg')
-      .attr('width', 500)
-      .attr('height', 500);
+    const svg = d3.select(element.current).append('svg').attr('width', 500).attr('height', 500);
 
-    svg
-      .selectAll('rect')
-      .data(data)
-      .enter()
-      .append('rect')
-      .attr('x', (d, i) => i * 100)
-      .attr('y', 50)
-      .attr('width', 50)
-      .attr('height', d => d * 10)
-      .attr('fill', 'lightgrey');
+    d3.json(url).then(data => {
+      svg
+        .selectAll('rect')
+        .data(data)
+        .enter()
+        .append('rect')
+        .attr('x', (d, i) => i * 100)
+        .attr('y', 50)
+        .attr('width', 50)
+        .attr('height', d => d.age * 10)
+        .attr('fill', d => (d.age > 10 ? 'red' : 'green'));
+    });
   }
 }
