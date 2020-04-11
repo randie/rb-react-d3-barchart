2a9ade1b0a04d979aa2efe9e7c09da4494e63203
Randie B <randie.bemis@gmail.com>
2020-04-11 01:21:06 -0700

    add x and y axes

--- src/components/d3-chart.js
+++ src/components/d3-chart.js
@@ -27,28 +27,37 @@ const url = 'http://localhost:4000/tallestMen';
 ]]
 */
 
-const WIDTH = 800;
-const HEIGHT = 500;
+const margin = { top: 10, right: 10, bottom: 50, left: 50 };
+const width = 800;
+const height = 500;
 
 export default class D3Chart {
   constructor(element) {
     const svg = d3
       .select(element.current)
       .append('svg')
-      .attr('width', WIDTH)
-      .attr('height', HEIGHT);
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', `translate(${margin.left}, ${margin.right})`);
 
     d3.json(url).then(data => {
-      const yScale = d3
-        .scaleLinear()
-        .domain([0, d3.max(data, d => d.height)])
-        .range([0, HEIGHT]);
-
       const xScale = d3
         .scaleBand()
         .domain(data.map(d => d.name))
-        .range([0, WIDTH])
+        .range([0, width])
         .padding(0.4);
+
+      const yScale = d3
+        .scaleLinear()
+        .domain([0, d3.max(data, d => d.height)])
+        .range([0, height]);
+
+      const xAxisGenerate = d3.axisBottom(xScale);
+      const yAxisGenerate = d3.axisLeft(yScale);
+
+      svg.append('g').attr('transform', `translate(0, ${height})`).call(xAxisGenerate);
+      svg.append('g').call(yAxisGenerate);
 
       svg
         .selectAll('rect')
@@ -56,7 +65,7 @@ export default class D3Chart {
         .enter()
         .append('rect')
         .attr('x', d => xScale(d.name))
-        .attr('y', d => HEIGHT - yScale(d.height))
+        .attr('y', d => height - yScale(d.height))
         .attr('width', xScale.bandwidth)
         .attr('height', d => yScale(d.height))
         .attr('fill', d => 'grey');
