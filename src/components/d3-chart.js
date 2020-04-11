79eed84607d1c828683b89829f1a3812332ea8cd
Randie B <randie.bemis@gmail.com>
2020-04-11 01:33:48 -0700

    refactor d3Chart to constructor() and update()
    constructor() will include code that only runs once and is independent of the data
    update() includes code that will run every time the data changes

--- src/components/d3-chart.js
+++ src/components/d3-chart.js
@@ -41,49 +41,60 @@ export default class D3Chart {
       .append('g')
       .attr('transform', `translate(${margin.left}, ${margin.right})`);
 
+    svg
+      .append('text')
+      .attr('x', width / 2)
+      .attr('y', height + 44)
+      .attr('text-anchor', 'middle')
+      .text("The world's tallest men"); // TODO
+
+    svg
+      .append('text')
+      .attr('x', -(height / 2))
+      .attr('y', -45)
+      .attr('text-anchor', 'middle')
+      .attr('transform', 'rotate(-90)')
+      .text('Height in cm');
+
+    this.xAxisGroup = svg.append('g').attr('transform', `translate(0, ${height})`);
+    this.yAxisGroup = svg.append('g');
+
+    this.svg = svg;
+
     d3.json(url).then(data => {
-      const xScale = d3
-        .scaleBand()
-        .domain(data.map(d => d.name))
-        .range([0, width])
-        .padding(0.4);
-
-      const yScale = d3
-        .scaleLinear()
-        .domain([d3.min(data, d => d.height) * 0.95, d3.max(data, d => d.height)])
-        .range([height, 0]);
-
-      const xAxisGenerate = d3.axisBottom(xScale).tickSize(0).tickPadding(6);
-      const yAxisGenerate = d3.axisLeft(yScale);
-
-      svg.append('g').attr('transform', `translate(0, ${height})`).call(xAxisGenerate);
-      svg.append('g').call(yAxisGenerate);
-
-      svg
-        .append('text')
-        .attr('x', width / 2)
-        .attr('y', height + 44)
-        .attr('text-anchor', 'middle')
-        .text("The world's tallest men");
-
-      svg
-        .append('text')
-        .attr('x', -(height / 2))
-        .attr('y', -45)
-        .attr('text-anchor', 'middle')
-        .attr('transform', 'rotate(-90)')
-        .text('Height in cm');
-
-      svg
-        .selectAll('rect')
-        .data(data)
-        .enter()
-        .append('rect')
-        .attr('x', d => xScale(d.name))
-        .attr('y', d => yScale(d.height))
-        .attr('width', xScale.bandwidth)
-        .attr('height', d => height - yScale(d.height))
-        .attr('fill', d => 'grey');
+      this.update(data);
     });
+  }
+
+  update(data) {
+    const { svg, xAxisGroup, yAxisGroup } = this;
+
+    const xScale = d3
+      .scaleBand()
+      .domain(data.map(d => d.name))
+      .range([0, width])
+      .padding(0.4);
+
+    const yScale = d3
+      .scaleLinear()
+      .domain([d3.min(data, d => d.height) * 0.95, d3.max(data, d => d.height)])
+      .range([height, 0]);
+
+    const xAxisGenerate = d3.axisBottom(xScale).tickSize(0).tickPadding(6);
+    const yAxisGenerate = d3.axisLeft(yScale);
+
+    xAxisGroup.call(xAxisGenerate);
+    yAxisGroup.call(yAxisGenerate);
+
+    svg
+      .selectAll('rect')
+      .data(data)
+      .enter()
+      .append('rect')
+      .attr('x', d => xScale(d.name))
+      .attr('y', d => yScale(d.height))
+      .attr('width', xScale.bandwidth)
+      .attr('height', d => height - yScale(d.height))
+      .attr('fill', d => 'grey');
   }
 }
