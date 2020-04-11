572e9a2956c87c70bd4d59726dbb434bda926261
Randie B <randie.bemis@gmail.com>
2020-04-11 01:26:34 -0700

    don't import app.css

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Chart from './chart';
-import '../styles/app.css'; // TODO: remove
 import Container from 'react-bootstrap/Container';
 
 function App() {
